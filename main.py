88478cd098a8b1b2c31ad6ce36f029b2d7f1554f
Ryan Huang <59319094+ryan-huang1@users.noreply.github.com>
2021-11-20 02:25:21 +0000

    removed try

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
+import requests
 import tweepy
+import json
+import os
 
 #variables for accessing twitter API
 consumer_key=os.environ['consumer_key']
@@ -8,16 +10,62 @@ access_token=os.environ['access_token']
 access_token_secret=os.environ['access_token_secret']
 
 def OAuth():
+    auth=tweepy.OAuthHandler(consumer_key, consumer_secret_key)
+    auth.set_access_token(access_token, access_token_secret)
+    return auth
 
-    try:
-        auth=tweepy.OAuthHandler(consumer_key, consumer_secret_key)
-        auth.set_access_token(access_token, access_token_secret)
-        return auth
-    except Exception as e:
-        return None
+#func to parse data
+def funcWeatherJSON():
+    weather = requests.get("https://mars.nasa.gov/rss/api/?feed=weather&category=mars2020&feedtype=json")
+    # processes the raw data into JSON
+    weatherJSON = json.loads(weather.text)
+    return(weatherJSON)
 
+def funcDateData():
+    weatherJSON = funcWeatherJSON()
+    tempatureData = weatherJSON['sols'][6]['terrestrial_date']
+    return(tempatureData)
+
+def funcSolData():
+    weatherJSON = funcWeatherJSON()
+    tempatureData = weatherJSON['sols'][6]['sol']
+    return(tempatureData)
+
+def funcSeasonData():
+    weatherJSON = funcWeatherJSON()
+    tempatureData = weatherJSON['sols'][6]['season']
+    return(tempatureData)
+
+def funcMinTempData():
+    weatherJSON = funcWeatherJSON()
+    tempatureData = weatherJSON['sols'][6]['min_temp']
+    return(tempatureData)
+
+def funcMaxTempData():
+    weatherJSON = funcWeatherJSON()
+    tempatureData = weatherJSON['sols'][6]['max_temp']
+    return(tempatureData)
+
+def funcPressureData():
+    weatherJSON = funcWeatherJSON()
+    tempatureData = weatherJSON['sols'][6]['pressure']
+    return(tempatureData)
+
+def funcSunriseData():
+    weatherJSON = funcWeatherJSON()
+    tempatureData = weatherJSON['sols'][6]['sunrise']
+    return(tempatureData)
+
+def funcSunsetData():
+    weatherJSON = funcWeatherJSON()
+    tempatureData = weatherJSON['sols'][6]['sunset']
+    return(tempatureData)
+
+
+#post tweet/status
 oauth = OAuth()
 api = tweepy.API(oauth)
 
-api.update_status('well there buddy')
+api.update_status('well there buddy hello testing')
 print('tweet posted')
+
